306afbbe65f5aa446a5d8cd3158f1a8e3d4494e9
RahulFernando <rahulrulz680@gmail.com>
2024-12-15 16:16:36 +0530

    App bar and hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,33 @@
-import type { Metadata } from "next";
+"use client";
 import "./globals.css";
 import { Nunito_Sans } from "@next/font/google";
+import { createTheme } from "./theme";
+import { ThemeProvider } from "@mui/material";
+import SnackbarProvider from "./contexts/snackbar/snackbar-context";
+import AppBar from "./components/app-bar";
 
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
   weight: ["400"],
 });
 
-export const metadata: Metadata = {
-  title: "Event Horizon",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const theme = createTheme();
+
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>{children}</body>
+      <body className={nunitoSans.className}>
+        <ThemeProvider theme={theme}>
+          <SnackbarProvider>
+            <AppBar />
+            {children}
+          </SnackbarProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
