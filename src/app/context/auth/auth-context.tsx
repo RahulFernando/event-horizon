6cd001f2451589f07a2117d1edcc2e298e895c3f
RahulFernando <rahulrulz680@gmail.com>
2024-12-29 18:37:21 +0530

    Redirrect page after successful login

--- src/app/context/auth/auth-context.tsx
+++ src/app/context/auth/auth-context.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { createContext, PropsWithChildren, useEffect, useReducer } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useReducer,
+} from "react";
 import {
   AuthAccount,
   AuthAction,
@@ -58,23 +64,20 @@ const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
     }
   }, [account, removeAccont, removeToken, token]);
 
-  const loginSuccessHandler = ({
-    token,
-    account,
-  }: {
-    token: string;
-    account: AuthAccount;
-  }) => {
-    setToken(token);
-    setAccount(account);
-    dispatch({ type: "LOGIN", payload: { token, account } });
-  };
+  const loginSuccessHandler = useCallback(
+    ({ token, account }: { token: string; account: AuthAccount }) => {
+      setToken(token);
+      setAccount(account);
+      dispatch({ type: "LOGIN", payload: { token, account } });
+    },
+    [setAccount, setToken]
+  );
 
-  const signOutHandler = () => {
+  const signOutHandler = useCallback(() => {
     removeToken();
     removeAccont();
     dispatch({ type: "SIGN_OUT" });
-  };
+  }, [removeAccont, removeToken]);
 
   const values = {
     ...authState,
