46614678eaa3ded5c57719a0e973197c605234cd
RahulFernando <rahulrulz680@gmail.com>
2025-02-26 21:13:37 +0530

    Display job status on selected vendor list item

--- src/app/my-events/components/selected-gigs/selected-gigs.types.ts
+++ src/app/my-events/components/selected-gigs/selected-gigs.types.ts
@@ -1,12 +1,19 @@
+import { JobStatus } from "@prisma/client";
 import { IGig } from "../../events.type";
 
 export interface SelectedGigItemProps {
   title: string;
   name: string;
   id: string;
+  status?: JobStatus;
   onDelete: (id: string, event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export interface SelectedGigsProps {
   gigs: IGig[];
+}
+
+export interface JobTitleProps {
+  title: string;
+  status: JobStatus;
 }
