eab91b54be426db6618945e72d7d4ab3b7ad730d
RahulFernando <rahulrulz680@gmail.com>
2025-02-26 21:24:22 +0530

    Add chat icon based on status

--- src/app/my-events/components/selected-gigs/selected-item.tsx
+++ src/app/my-events/components/selected-gigs/selected-item.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IconButton, ListItem, ListItemText, Tooltip } from "@mui/material";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import ChatIcon from "@mui/icons-material/Chat";
 import { SelectedGigItemProps } from "./selected-gigs.types";
 import JobTitle from "./job-title";
 
@@ -8,14 +9,21 @@ const SelectedGig: React.FC<SelectedGigItemProps> = ({
   title,
   name,
   id,
-  status = "PENDING",
+  status = "ACCEPTED",
   onDelete,
 }) => (
   <ListItem
     secondaryAction={
-      <Tooltip title="Delete" placement="left-start">
-        <IconButton edge="end" color="error" onClick={onDelete.bind(this, id)}>
-          <DeleteOutlineIcon />
+      <Tooltip
+        title={status === "ACCEPTED" ? "Chat" : "Delete"}
+        placement="left-start"
+      >
+        <IconButton
+          edge="end"
+          color={status === "ACCEPTED" ? "default" : "error"}
+          onClick={status === "ACCEPTED" ? undefined : onDelete.bind(this, id)}
+        >
+          {status === "ACCEPTED" ? <ChatIcon /> : <DeleteOutlineIcon />}
         </IconButton>
       </Tooltip>
     }
