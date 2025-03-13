fd2a4181a598c42184958bc7a25048f2ebbacc92
RahulFernando <rahulrulz680@gmail.com>
2025-03-13 14:58:31 +0530

    Configure chat navigation

--- src/app/api/vendors/[id]/gigs/route.ts
+++ src/app/api/vendors/[id]/gigs/route.ts
@@ -1,6 +1,7 @@
+import { withAuth } from "@/lib/auth-utils";
 import prisma from "@/lib/prisma";
 import { createGigValidationSchema } from "@/lib/validations/gigs/create-validation-schema";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { ValidationError } from "yup";
 
 export async function GET(
@@ -38,54 +39,58 @@ export async function POST(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const body = await req.json();
-  const { id } = await params;
+  return withAuth(req as NextRequest, async (req, user) => {
+    const userName = user.name;
 
-  try {
-    await createGigValidationSchema.validate(
-      { ...body, vendor_id: id },
-      { abortEarly: true }
-    );
+    const body = await req.json();
+    const { id } = await params;
 
-    const { event_type_ids } = body;
+    try {
+      await createGigValidationSchema.validate(
+        { ...body, vendor_id: id },
+        { abortEarly: true }
+      );
 
-    const gig = await prisma.gig.create({
-      data: {
-        title: body.title,
-        description: body.description,
-        location: body.location,
-        category_id: body.category_id,
-        vendor_id: id,
-        created_by: "unauthorized user",
-        updated_by: "unauthorized user",
-        event_types: {
-          create: event_type_ids.map((id: string) => ({
-            event_type: {
-              connect: { id },
-            },
-          })),
+      const { event_type_ids } = body;
+
+      const gig = await prisma.gig.create({
+        data: {
+          title: body.title,
+          description: body.description,
+          location: body.location,
+          category_id: body.category_id,
+          vendor_id: id,
+          created_by: userName,
+          updated_by: userName,
+          event_types: {
+            create: event_type_ids.map((id: string) => ({
+              event_type: {
+                connect: { id },
+              },
+            })),
+          },
         },
-      },
-      select: {
-        id: true,
-        title: true,
-        description: true,
-        location: true,
-        category_id: true,
-        blob_url: true,
-        event_types: {
-          select: { event_type: { select: { id: true, name: true } } },
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          location: true,
+          category_id: true,
+          blob_url: true,
+          event_types: {
+            select: { event_type: { select: { id: true, name: true } } },
+          },
+          vendor_id: true,
         },
-        vendor_id: true,
-      },
-    });
+      });
 
-    return NextResponse.json(gig, { status: 201 });
-  } catch (error) {
-    if (error instanceof ValidationError) {
-      return NextResponse.json({ errors: error.errors }, { status: 400 });
+      return NextResponse.json(gig, { status: 201 });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return NextResponse.json({ errors: error.errors }, { status: 400 });
+      }
+
+      return NextResponse.json({ errors: [error] }, { status: 500 });
     }
-
-    return NextResponse.json({ errors: [error] }, { status: 500 });
-  }
+  });
 }
--- src/app/my-events/components/selected-gigs/selected-item.tsx
+++ src/app/my-events/components/selected-gigs/selected-item.tsx
@@ -4,6 +4,7 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import ChatIcon from "@mui/icons-material/Chat";
 import { SelectedGigItemProps } from "./selected-gigs.types";
 import JobTitle from "./job-title";
+import { useRouter } from "next/navigation";
 
 const SelectedGig: React.FC<SelectedGigItemProps> = ({
   title,
@@ -11,28 +12,36 @@ const SelectedGig: React.FC<SelectedGigItemProps> = ({
   id,
   status = "ACCEPTED",
   onDelete,
-}) => (
-  <ListItem
-    secondaryAction={
-      <Tooltip
-        title={status === "ACCEPTED" ? "Chat" : "Delete"}
-        placement="left-start"
-      >
-        <IconButton
-          edge="end"
-          color={status === "ACCEPTED" ? "default" : "error"}
-          onClick={status === "ACCEPTED" ? undefined : onDelete.bind(this, id)}
+}) => {
+  const router = useRouter();
+
+  const chatClickHandler = () => router.push("/chat");
+
+  return (
+    <ListItem
+      secondaryAction={
+        <Tooltip
+          title={status === "ACCEPTED" ? "Chat" : "Delete"}
+          placement="left-start"
         >
-          {status === "ACCEPTED" ? <ChatIcon /> : <DeleteOutlineIcon />}
-        </IconButton>
-      </Tooltip>
-    }
-  >
-    <ListItemText
-      primary={<JobTitle title={title} status={status} />}
-      secondary={name}
-    />
-  </ListItem>
-);
+          <IconButton
+            edge="end"
+            color={status === "ACCEPTED" ? "default" : "error"}
+            onClick={
+              status === "ACCEPTED" ? chatClickHandler : onDelete.bind(this, id)
+            }
+          >
+            {status === "ACCEPTED" ? <ChatIcon /> : <DeleteOutlineIcon />}
+          </IconButton>
+        </Tooltip>
+      }
+    >
+      <ListItemText
+        primary={<JobTitle title={title} status={status} />}
+        secondary={name}
+      />
+    </ListItem>
+  );
+};
 
 export default SelectedGig;
